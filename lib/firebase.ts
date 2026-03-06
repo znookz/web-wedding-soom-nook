ba4715b1a50c5a09e3b0aa3dfa51c64192dadd49
Pattarapon <pattarapon.kasco@gmail.com>
2026-03-07 01:21:31 +0700

    โครงสร้าง

---
+++ lib/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
